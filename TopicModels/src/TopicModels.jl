a0925e14775fa7384ed2e4760bef2532e72affa8
Haseeb <haseebulr@yahoo.com>
2020-11-24 15:40:49 +0900

    Organize the LDA into a package

---
+++ TopicModels/src/TopicModels.jl
@@ -0,0 +1,19 @@
+module TopicModels
+
+greet() = print("Hello World!")
+
+include("DocumentSet.jl")
+export documentset_readData
+export documentset_addDocument
+export documentset_transform
+export documentset_sampleDocuments
+export documentset_OnlinesampleDocuments
+export documentset_getTermFreq
+
+include("Dirichlet.jl")
+export dirichlet_optimizeParam
+export dirichlet_set
+
+include("Polya")
+
+end # module
